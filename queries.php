c5cddff8ab5db236501ca00898a97228cc8b5d6d
Andrew Moll <mollandrew913@gmail.com>
2019-04-13 21:46:47 +1200

    Remove queries.php

--- queries.php
+++
@@ -1,4 +0,0 @@
-<?php
-$userquery = "SELECT * FROM tbuser WHERE user_name LIKE :user_name";
-$registerquery = "INSERT INTO `tbuser`(`user_name`, `first_name`,`last_name`, `email`, `password`) VALUES (:user_name, :first_name, :last_name, :email, :password)";
-?>
