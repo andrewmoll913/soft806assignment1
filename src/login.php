e809a8cbbe5e7f5b95c8f045c593695e76d6bf5f
Andrew Moll <mollandrew913@gmail.com>
2019-04-13 21:18:08 +1200

    Use the user class for login.

--- src/login.php
+++ src/login.php
@@ -1,37 +1,23 @@
 <?php
-require_once "db.php";
-require_once "queries.php";
+require_once "User.php";
 
-if(!empty($_POST["user_name"]) && !empty($_POST["password"])) {
-	try {
-		
-		$db = makeDB();
-		$query = $db->prepare($userquery);
-
-		$query->bindParam(":user_name", $_POST["user_name"]);
-		$query->execute();
-		$user = $query->fetch(PDO::FETCH_ASSOC);
-
-		if($user == false) {
-			http_response_code(404);
-			header("Content-Type: text/html");
-		} else if(password_verify($_POST["password"], $user["password"])) {
-			http_response_code(401);
-			header("Content-Type: application/json");
-			echo "Unauthorized ";
-		}  else {
-			session_start();
-			$_SESSION["isvaliduser"] = 0;
-			$_SESSION["user_name"] = $user["user_name"];
-			unset($user["password"]);
-			$_SESSION["isvaliduser"] = 1;
-                                header("Location: main.php",  true, 303);
-                                die();
-		}
-		
-		$db = null;
-	} catch (PDOException $e) {
-		echo 'Message: ' .$e->getMessage();
-	}
+if(!empty($_POST["user_name"]) && !empty($_POST["password"]))
+{
+    $status = User->login($_POST["user_name"], $_POST["password"]);
+    if($status)
+    {
+        http_response_code(404);
+        header("Content-Type: text/html");
+    }
+    else
+    {
+        session_start();
+        $_SESSION["isvaliduser"] = 0;
+        $_SESSION["user_name"] = $_POST["user_name"];
+        unset($user["password"]);
+        $_SESSION["isvaliduser"] = 1;
+        header("Location: main.php",  true, 303);
+        die();
+    }
 }
 ?>
